21c7f55096e2aee198d957ee2e9e186ecbd4da36
elena <elena.15@hotmail.it>
2015-02-20 17:03:22 +0000

    third test is passing

--- Answer.js
+++ Answer.js
@@ -1,9 +1,22 @@
 var $ = function (selector) {
   var elements = [];
 
-  tag = document.getElementsByTagName(selector)
-  for (i = 0; i < tag.length; i++){
-    elements.push(tag[i])
+  if (selector.indexOf('#') > -1){
+    selector = selector.split('#');
+    selector = selector[selector.length -1];
+    id = document.getElementById(selector);
+    if (id !== null){
+      elements.push(id)}
+    // }else{
+    //   elements = []
+    // }
+  }
+
+  if (selector !== undefined){
+    tag = document.getElementsByTagName(selector)
+    for (i = 0; i < tag.length; i++){
+      elements.push(tag[i])
+    }
   }
 
   return elements;
