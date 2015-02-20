694e173dc767f0a7e0b1023eefa087c9e9998fef
elena <elena.15@hotmail.it>
2015-02-20 13:44:50 +0000

    fixed search for element in a nicer way

--- Answer.js
+++ Answer.js
@@ -1,7 +1,10 @@
 var $ = function (selector) {
   var elements = [];
 
-  elements = document.getElementsByTagName(selector)
+  tag = document.getElementsByTagName(selector)
+  for (i = 0; i < tag.length; i++){
+    elements.push(tag[i])
+  }
 
   return elements;
 }
