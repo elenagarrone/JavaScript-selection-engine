0c3841588b7c0b12c3da84aba881397e3ba62e0e
elena <elena.15@hotmail.it>
2015-02-21 10:58:26 +0000

    test d is now passing

--- Answer.js
+++ Answer.js
@@ -2,31 +2,44 @@ var $ = function (selector) {
   var elements = [];
 
   if (selector.indexOf('#') > -1){
-    selector = selector.split('#');
-    selector = selector[selector.length -1];
+    array = selector.split('#');
+    selector = array[array.length -1];
+    if(selector.indexOf('.') > -1){
+      selector = selector.split('.');
+      selector = selector[0];
+    }
     id = document.getElementById(selector);
     if (id !== null){
       elements.push(id)
     }
-  }
+  } 
 
-  if (selector.indexOf('.') > -1){
-    selector = selector.split('.');
-    selector = selector[selector.length -1];
-    classes = document.getElementsByClassName(selector);
-    if (classes !== null){
-      for (i = 0; i < classes.length; i++){
-        elements.push(classes[i])
+  if (selector.indexOf('.') > -1 && selector.indexOf('#') === -1){
+    array = selector.split('.');
+    selector = array[array.length -1];
+    if (array[0] === ''){
+      classes = document.getElementsByClassName(selector);
+      if (classes !== null){
+        for (i = 0; i < classes.length; i++){
+          elements.push(classes[i])
+        }
+      }
+    } else {
+      classes = document.getElementsByClassName(selector) && document.getElementsByTagName(array[0]);
+      if (classes !== null){
+        for (i = 0; i < classes.length; i++){
+          elements.push(classes[i])
+        }
       }
     }
   }
 
-  if (selector !== undefined){
-    tag = document.getElementsByTagName(selector)
-    for (i = 0; i < tag.length; i++){
-      elements.push(tag[i])
-    }
+  tag = document.getElementsByTagName(selector)
+  for (i = 0; i < tag.length; i++){
+    elements.push(tag[i])
   }
 
   return elements;
 }
+
+// realArray = Array.prototype.slice.call(classes)
