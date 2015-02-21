e7891af95a7ea7c47203ca917241cc195949e174
elena <elena.15@hotmail.it>
2015-02-21 18:06:59 +0000

    remove unused code in the class block

--- Answer.js
+++ Answer.js
@@ -3,12 +3,12 @@ var $ = function (selector) {
 
   if (selector.indexOf('#') > -1){
     var selectors = selector.split('#');
-    var id_selector = selectors[selectors.length -1];
+    var selector = selectors[selectors.length -1];
     var tag_selector = selectors[0]
-    if(id_selector.indexOf('.') > -1){
-      var id_selector = id_selector.split('.')[0];
+    if(selector.indexOf('.') > -1){
+      var selector = selector.split('.')[0];
     } 
-    var element_with_id = document.getElementById(id_selector);
+    var element_with_id = document.getElementById(selector);
     var elements_with_tag = document.getElementsByTagName(tag_selector);
     if (elements_with_tag.length > 0){
       for (i = 0; i < elements_with_tag.length; i++){
@@ -24,31 +24,32 @@ var $ = function (selector) {
   } 
 
   if (selector.indexOf('.') > -1 && selector.indexOf('#') === -1){
-    array = selector.split('.');
-    selector = array[array.length -1];
-    if (array[0] === ''){
-      classes = document.getElementsByClassName(selector);
-      if (classes !== null){
-        for (i = 0; i < classes.length; i++){
-          elements.push(classes[i])
+    var selectors = selector.split('.');
+    var selector = selectors[selectors.length -1];
+    var tag_selector = selectors[0]
+    var elements_with_class = document.getElementsByClassName(selector);
+    if (tag_selector === ''){
+      if (elements_with_class !== null){
+        for (i = 0; i < elements_with_class.length; i++){
+          elements.push(elements_with_class[i])
         }
       }
     } else {
-      classes = document.getElementsByClassName(selector) && document.getElementsByTagName(array[0]);
-      if (classes !== null){
-        for (i = 0; i < classes.length; i++){
-          elements.push(classes[i])
+      var elements_with_tag = document.getElementsByTagName(tag_selector)
+      elements_with_class_and_tag = elements_with_class && elements_with_tag;
+      if (elements_with_class_and_tag !== null){
+        for (i = 0; i < elements_with_class_and_tag.length; i++){
+          elements.push(elements_with_class_and_tag[i])
         }
       }
     }
   }
 
-  tag = document.getElementsByTagName(selector)
-  for (i = 0; i < tag.length; i++){
-    elements.push(tag[i])
+  var elements_with_tag = document.getElementsByTagName(selector)
+  for (i = 0; i < elements_with_tag.length; i++){
+    elements.push(elements_with_tag[i])
   }
 
   return elements;
 }
 
-// realArray = Array.prototype.slice.call(classes)
