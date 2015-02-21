a8a95f769ac16543cd0a0d99faafdb50cb879e98
elena <elena.15@hotmail.it>
2015-02-21 17:03:52 +0000

    remove unused code in the id block

--- Answer.js
+++ Answer.js
@@ -2,30 +2,25 @@ var $ = function (selector) {
   var elements = [];
 
   if (selector.indexOf('#') > -1){
-    array = selector.split('#');
-    selector = array[array.length -1];
-    if(selector.indexOf('.') > -1){
-      selector = selector.split('.');
-      selector = selector[0];
-    } else if (array[0].indexOf('.') > -1){
-      array = array[0].split('.'); 
-    }
-
-    id = document.getElementById(selector);
-    tag = document.getElementsByTagName(array[0])
-    if (tag.length > 0){
-      for (i = 0; i < tag.length; i++){
-        if (id === tag[i] || array[0] === ''){
-          elements.push(id)
+    var selectors = selector.split('#');
+    var id_selector = selectors[selectors.length -1];
+    var tag_selector = selectors[0]
+    if(id_selector.indexOf('.') > -1){
+      var id_selector = id_selector.split('.')[0];
+    } 
+    var element_with_id = document.getElementById(id_selector);
+    var elements_with_tag = document.getElementsByTagName(tag_selector);
+    if (elements_with_tag.length > 0){
+      for (i = 0; i < elements_with_tag.length; i++){
+        if (element_with_id === elements_with_tag[i]){
+          elements.push(element_with_id)
         } else {
           elements = []
         }
       }
     } else {
-      elements.push(id)
+      elements.push(element_with_id)
     }
-
-
   } 
 
   if (selector.indexOf('.') > -1 && selector.indexOf('#') === -1){
