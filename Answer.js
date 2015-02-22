4b867579898639e00123e096355cc1fe64acbe7f
elena <elena.15@hotmail.it>
2015-02-22 15:27:38 +0000

    did most of the refactoring for the id block

--- Answer.js
+++ Answer.js
@@ -1,49 +1,64 @@
 var $ = function (selector) {
   var elements = [];
 
-  var dividingBy = function(id_or_class){
-    selectors = selector.split(id_or_class);
-    selector = selectors[selectors.length -1];
-    tag_selector = selectors[0]
+  var getSelectors = function(id_or_class){
+    splitWhenFind(id_or_class)
+    getSelector(selectors);
+    getTagSelector(selectors);
   }
+
+  var splitWhenFind = function(id_or_class){
+    selectors = selector.split(id_or_class);
+  }
+
+  var getSelector = function(selectors){
+    selector = selectors[selectors.length -1];
+  }
+
+  var getTagSelector = function(selectors){
+    tag_selector = selectors[0];
+  }
+
+  var getIdSelector = function(){
+    selector = selector.split('.')[0]; 
+  }
+
   var addEachElementOf = function(array){
     for (i = 0; i < array.length; i++){
-      elements.push(array[i])
+      elements.push(array[i]);
+    }
+  }
+
+  var elementsWithSame = function(id, tag){
+    for (i = 0; i < tag.length; i++){
+      if (id === tag[i]){
+        elements.push(id);
+      } else {
+        elements = [];
+      }
     }
   }
 
   if (selector.indexOf('#') > -1){
-    dividingBy('#')
-    if(selector.indexOf('.') > -1){ 
-      var selector = selector.split('.')[0]; 
-    } 
-    var element_with_id = document.getElementById(selector);
-    var elements_with_tag = document.getElementsByTagName(tag_selector);
-    if (elements_with_tag.length > 0){
-      for (i = 0; i < elements_with_tag.length; i++){
-        if (element_with_id === elements_with_tag[i]){
-          elements.push(element_with_id)
-        } else {
-          elements = []
-        }
-      }
-    } else {
-      elements.push(element_with_id)
-    }
+    getSelectors('#');
+    if(selector.indexOf('.') > -1){ getIdSelector() } 
+    var element_id = document.getElementById(selector);
+    var elements_tag = document.getElementsByTagName(tag_selector);
+    elements_tag.length > 0 ? elementsWithSame(element_id, elements_tag) : elements.push(element_id)
   } 
 
   if (selector.indexOf('.') > -1 && selector.indexOf('#') === -1){
-    dividingBy('.')
+    getSelectors('.');
     var elements_with_class = document.getElementsByClassName(selector);
     if (tag_selector === ''){
       if (elements_with_class !== null){
-        addEachElementOf(elements_with_class)
+        addEachElementOf(elements_with_class);
       }
     } else {
       var elements_with_tag = document.getElementsByTagName(tag_selector)
       elements_with_class_and_tag = elements_with_class && elements_with_tag;
       if (elements_with_class_and_tag !== null){
-        addEachElementOf(elements_with_class_and_tag)
+        addEachElementOf(elements_with_class_and_tag);
       }
     }
   }
