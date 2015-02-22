1a4d41b597413d52ff8331c44b41a5f3920a20e1
elena <elena.15@hotmail.it>
2015-02-22 14:12:49 +0000

    doing some refactoring. Added dividingBy and addEachElementsOf function

--- Answer.js
+++ Answer.js
@@ -1,11 +1,22 @@
 var $ = function (selector) {
   var elements = [];
 
+  var dividingBy = function(id_or_class){
+    selectors = selector.split(id_or_class);
+    selector = selectors[selectors.length -1];
+    tag_selector = selectors[0]
+  }
+  var addEachElementOf = function(array){
+    for (i = 0; i < array.length; i++){
+      elements.push(array[i])
+    }
+  }
+
   if (selector.indexOf('#') > -1){
-    var selectors = selector.split('#');
-    var selector = selectors[selectors.length -1];
-    var tag_selector = selectors[0]
-    if(selector.indexOf('.') > -1){ var selector = selector.split('.')[0]; } 
+    dividingBy('#')
+    if(selector.indexOf('.') > -1){ 
+      var selector = selector.split('.')[0]; 
+    } 
     var element_with_id = document.getElementById(selector);
     var elements_with_tag = document.getElementsByTagName(tag_selector);
     if (elements_with_tag.length > 0){
@@ -22,31 +33,23 @@ var $ = function (selector) {
   } 
 
   if (selector.indexOf('.') > -1 && selector.indexOf('#') === -1){
-    var selectors = selector.split('.');
-    var selector = selectors[selectors.length -1];
-    var tag_selector = selectors[0]
+    dividingBy('.')
     var elements_with_class = document.getElementsByClassName(selector);
     if (tag_selector === ''){
       if (elements_with_class !== null){
-        for (i = 0; i < elements_with_class.length; i++){
-          elements.push(elements_with_class[i])
-        }
+        addEachElementOf(elements_with_class)
       }
     } else {
       var elements_with_tag = document.getElementsByTagName(tag_selector)
       elements_with_class_and_tag = elements_with_class && elements_with_tag;
       if (elements_with_class_and_tag !== null){
-        for (i = 0; i < elements_with_class_and_tag.length; i++){
-          elements.push(elements_with_class_and_tag[i])
-        }
+        addEachElementOf(elements_with_class_and_tag)
       }
     }
   }
 
   var elements_with_tag = document.getElementsByTagName(selector)
-  for (i = 0; i < elements_with_tag.length; i++){
-    elements.push(elements_with_tag[i])
-  }
+  addEachElementOf(elements_with_tag)
 
   return elements;
 }
